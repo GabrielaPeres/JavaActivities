07704d37a3b4e31ff37e098cd93bb18e437bffe7
GabrielaPeres <gabrieladperes1998@gmail.com>
2022-10-31 20:02:52 -0300

    @GabrielaPeres [Task:Aula11 ] {31/10/2022-20:03}

---
+++ Aula011/src/aula011/Aula011.java
@@ -0,0 +1,27 @@
+
+package aula011;
+
+
+public class Aula011 {
+
+   
+    public static void main(String[] args) {
+       
+        Visitante v1 = new Visitante();
+        Aluno a1 = new Aluno();
+        Bolsista b1 = new Bolsista();
+        
+        v1.setNome("Vanessa");
+        v1.setIdade(18);
+        v1.setSexo("feminino");
+        
+        a1.setNome("Fernanda");
+        a1.setIdade(15);
+        a1.setSexo("feminino");
+        
+        b1.setNome("Vitória");
+        b1.setIdade(12);
+        b1.setSexo("feminino");
+    }
+    
+}
---
+++ Aula011/src/aula011/Professor.java
@@ -0,0 +1,6 @@
+package aula011;
+
+
+public class Professor {
+    
+}
